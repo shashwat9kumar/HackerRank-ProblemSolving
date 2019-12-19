15bbdf9a5828e417e2821ddad86a1830f9104cf1
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2019-12-19 19:36:10 +0530

    Create Min-Max_Sum.cpp

---
+++ Min-Max_Sum.cpp
@@ -0,0 +1,133 @@
+/*
+
+
+Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+For example, . Our minimum sum is  and our maximum sum is . We would print
+
+16 24
+Function Description
+
+Complete the miniMaxSum function in the editor below. It should print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+miniMaxSum has the following parameter(s):
+
+arr: an array of  integers
+Input Format
+
+A single line of five space-separated integers.
+
+Constraints
+
+
+Output Format
+
+Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+Sample Input
+
+1 2 3 4 5
+Sample Output
+
+10 14
+Explanation
+
+Our initial numbers are , , , , and . We can calculate the following sums using four of the five integers:
+
+If we sum everything except , our sum is .
+If we sum everything except , our sum is .
+If we sum everything except , our sum is .
+If we sum everything except , our sum is .
+If we sum everything except , our sum is .
+Hints: Beware of integer overflow! Use 64-bit Integer.
+
+
+*/
+
+
+
+
+
+
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the miniMaxSum function below.
+void miniMaxSum(vector<int> arr) {
+    long int max=0, min=0;
+    for(int i=0;i<arr.size()-1;i++)
+    {
+        for(int j=0;j<arr.size()-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<4;i++)
+    {
+        min+=arr[i];
+    }
+    for(int i=1;i<5;i++)
+    {
+        max+=arr[i];
+    }
+    cout<<min<<" "<<max<<endl;
+
+
+}
+
+int main()
+{
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split_string(arr_temp_temp);
+
+    vector<int> arr(5);
+
+    for (int i = 0; i < 5; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    miniMaxSum(arr);
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
