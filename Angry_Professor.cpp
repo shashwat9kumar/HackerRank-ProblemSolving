8569f81575a9759a62515f6f4f47f938361ff8ff
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2019-12-19 22:57:29 +0530

    Create Angry_Professor.cpp

---
+++ Angry_Professor.cpp
@@ -0,0 +1,161 @@
+/*
+
+
+A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, he decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time () to arrived late ().
+
+Given the arrival time of each student and a threshhold number of attendees, determine if the class is canceled.
+
+Input Format
+
+The first line of input contains , the number of test cases.
+
+Each test case consists of two lines.
+
+The first line has two space-separated integers,  and , the number of students (size of ) and the cancellation threshold.
+The second line contains  space-separated integers () describing the arrival times for each student.
+
+Note: Non-positive arrival times () indicate the student arrived early or on time; positive arrival times () indicate the student arrived  minutes late.
+
+For example, there are  students who arrive at times . Four are there on time, and two arrive late. If there must be  for class to go on, in this case the class will continue. If there must be , then class is cancelled.
+
+Function Description
+
+Complete the angryProfessor function in the editor below. It must return YES if class is cancelled, or NO otherwise.
+
+angryProfessor has the following parameter(s):
+
+k: the threshold number of students
+a: an array of integers representing arrival times
+Constraints
+
+Output Format
+
+For each test case, print the word YES if the class is canceled or NO if it is not.
+
+Note
+If a student arrives exactly on time , the student is considered to have entered before the class started.
+
+Sample Input
+
+2
+4 3
+-1 -3 4 2
+4 2
+0 -1 2 1
+Sample Output
+
+YES
+NO
+Explanation
+
+For the first test case, . The professor wants at least  students in attendance, but only  have arrived on time ( and ) so the class is cancelled.
+
+For the second test case, . The professor wants at least  students in attendance, and there are  who have arrived on time ( and ) so the class is not cancelled.
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the angryProfessor function below.
+string angryProfessor(int k, vector<int> a) {
+    int n=a.size();
+    int num=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]<=0)
+        {
+            num++;
+        }
+    }
+    if(num<k)
+    return "YES";
+    else
+    return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string nk_temp;
+        getline(cin, nk_temp);
+
+        vector<string> nk = split_string(nk_temp);
+
+        int n = stoi(nk[0]);
+
+        int k = stoi(nk[1]);
+
+        string a_temp_temp;
+        getline(cin, a_temp_temp);
+
+        vector<string> a_temp = split_string(a_temp_temp);
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            int a_item = stoi(a_temp[i]);
+
+            a[i] = a_item;
+        }
+
+        string result = angryProfessor(k, a);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
+
