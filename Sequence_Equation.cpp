2030e0052520d45370a28b7dd0caac3a5a1752bf
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2019-12-26 19:03:01 +0530

    Update Sequence_Equation.cpp

--- Sequence_Equation.cpp
+++ Sequence_Equation.cpp
@@ -70,3 +70,99 @@ Sample Output 1
 
 
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+int index(vector<int> p, int ele)
+{
+    int n=p.size();
+    for(int i=0;i<n;i++)
+    {
+        if(p[i]==ele)
+        return (i+1);
+    }
+    return -1;
+}
+
+// Complete the permutationEquation function below.
+vector<int> permutationEquation(vector<int> p) {
+    int n=p.size();
+    vector<int> arr;
+    for(int i=1;i<=n;i++)
+    {
+        int loc1=index(p,i);
+        int loc2=index(p,loc1);
+        arr.push_back(loc2);
+    }
+    return arr;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string p_temp_temp;
+    getline(cin, p_temp_temp);
+
+    vector<string> p_temp = split_string(p_temp_temp);
+
+    vector<int> p(n);
+
+    for (int i = 0; i < n; i++) {
+        int p_item = stoi(p_temp[i]);
+
+        p[i] = p_item;
+    }
+
+    vector<int> result = permutationEquation(p);
+
+    for (int i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
