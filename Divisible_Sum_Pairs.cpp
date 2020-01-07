ec0f46e221e903a3ae1535246c0548413ee6bf17
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-07 22:11:37 +0530

    Update Divisible_Sum_Pairs.cpp

--- Divisible_Sum_Pairs.cpp
+++ Divisible_Sum_Pairs.cpp
@@ -62,3 +62,20 @@ int divisibleSumPairs(int n, int k, vector<int> ar) {
     }
     return count;
 }
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string nk_temp;
+    getline(cin, nk_temp);
+
+    vector<string> nk = split_string(nk_temp);
+
+    int n = stoi(nk[0]);
+
+    int k = stoi(nk[1]);
+
+    string ar_temp_temp;
+    getline(cin, ar_temp_temp);
+
+    vector<string> ar_temp = split_string(ar_temp_temp);
