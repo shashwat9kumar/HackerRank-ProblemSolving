a71cb09881910bbed6d78169c264fdce437b3128
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-07 22:12:01 +0530

    Update Divisible_Sum_Pairs.cpp

--- Divisible_Sum_Pairs.cpp
+++ Divisible_Sum_Pairs.cpp
@@ -79,3 +79,50 @@ int main()
     getline(cin, ar_temp_temp);
 
     vector<string> ar_temp = split_string(ar_temp_temp);
+ 
+     vector<int> ar(n);
+
+    for (int i = 0; i < n; i++) {
+        int ar_item = stoi(ar_temp[i]);
+
+        ar[i] = ar_item;
+    }
+
+    int result = divisibleSumPairs(n, k, ar);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
+
