d70f505525573aceb015cd3f83b6e04aa4e7a038
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-09 10:27:50 +0530

    Update Breaking_the_Record.cpp

--- Breaking_the_Record.cpp
+++ Breaking_the_Record.cpp
@@ -2,7 +2,9 @@
 
 
 
+Maria plays college basketball and wants to go pro. Each season she maintains a record of her play. She tabulates the number of times she breaks her season record for most points and least points in a game. Points scored in the first game establish her record for the season, and she begins counting from there.
 
+For example, assume her scores for the season are represented in the array . Scores are in the same order as the games played. She would tabulate her results as follows:
 
 
 */
