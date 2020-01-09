a4d7437587bb6572d15dca4a6cae93bc54024de5
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-09 10:28:21 +0530

    Update Breaking_the_Record.cpp

--- Breaking_the_Record.cpp
+++ Breaking_the_Record.cpp
@@ -6,5 +6,17 @@ Maria plays college basketball and wants to go pro. Each season she maintains a 
 
 For example, assume her scores for the season are represented in the array . Scores are in the same order as the games played. She would tabulate her results as follows:
 
+                                 Count
+Game  Score  Minimum  Maximum   Min Max
+ 0      12     12       12       0   0
+ 1      24     12       24       0   1
+ 2      10     10       24       1   1
+ 3      24     10       24       1   1
+Given Maria's scores for a season, find and print the number of times she breaks her records for most and least points scored during the season.
+
+Function Description
+
+
+
 
 */
