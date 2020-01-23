84bff6f9b3b6c264e28d13b52a7322c2d4be26ee
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-23 23:19:48 +0530

    Update Forming_a_Magic_Square.java

--- Forming_a_Magic_Square.java
+++ Forming_a_Magic_Square.java
@@ -69,3 +69,108 @@ then the total cost will be .
 
 
 */
+
+
+
+
+
+
+
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the formingMagicSquare function below.
+    static int formingMagicSquare(int[][] s) {
+
+        int arr[][]={{8,1,6,3,5,7,4,9,2},
+        {6,1,8,7,5,3,2,9,4},
+        {4,9,2,3,5,7,8,1,6},
+        {2,9,4,7,5,3,6,1,8},
+        {8,3,4,1,5,9,6,7,2},
+        {4,3,8,9,5,1,2,7,6},
+        {6,7,2,1,5,9,8,3,4},
+        {2,7,6,9,5,1,4,3,8}};
+
+
+
+        int orig[]=new int[9];
+        int x=0;
+
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                orig[x++]=s[i][j];
+            }
+        }
+
+        int cost=100;
+        int count=-1;
+
+        for(int i=0;i<8;i++)
+        { 
+            int cost2=0;
+            for(int j=0;j<9;j++)
+            {
+                int diff=arr[i][j]-orig[j];
+                if(diff != 0)
+                {
+                    cost2+=(int)Math.abs(diff);
+                }
+            }
+            if(cost2<cost)
+            {
+                cost=cost2;
+                count++;
+            }
+        }
+
+
+        for(int i=0;i<9;i++)
+        {
+            System.out.print(arr[count][i]+" ");
+            if(i==2 || i==5 || i==8)
+            {
+                System.out.println();
+            }
+        }
+        return cost;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[][] s = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            String[] sRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 3; j++) {
+                int sItem = Integer.parseInt(sRowItems[j]);
+                s[i][j] = sItem;
+            }
+        }
+
+        int result = formingMagicSquare(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
