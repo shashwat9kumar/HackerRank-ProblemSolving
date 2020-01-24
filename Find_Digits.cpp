2b9262299305721640db208ca97d53435f7e8e1e
Shashwat Kumar <43718923+shashwat9kumar@users.noreply.github.com>
2020-01-24 22:57:07 +0530

    Update Find_Digits.cpp

--- Find_Digits.cpp
+++ Find_Digits.cpp
@@ -52,3 +52,63 @@ The number  is broken into four digits, , , , and .  is evenly divisible by its 
 
 
 
+
+
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the findDigits function below.
+int findDigits(int n) {
+
+
+    int count=0;
+    int temp=n;
+    while(temp>0)
+    {
+        int d=temp%10;
+        if(d==0)
+        {
+            ;
+        }
+        else
+        {
+            if(n%d==0)
+            {
+                count++;
+            }
+        }
+        temp=temp/10;
+    }
+    return count;
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = findDigits(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+
+
